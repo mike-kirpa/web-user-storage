7726f7f84a9b876246ae7f6367a625e14d1c6f90
Mikhail Kyrpa <michael.kirpa@gmail.com>
2021-12-15 20:56:17 +0200

    some draft of app

--- web-serv.go
+++ web-serv.go
@@ -2,85 +2,39 @@ package main
 
 import (
 	"html/template"
-	"log"
 	"net/http"
-	"os"
-	"regexp"
+	"time"
 )
 
-type Page struct {
-	Title string
-	Body  []byte
+type User struct {
+	Index int
+	Name  string
+	Email string
+	Date  time.Time
+	Dates string
 }
+type Users []User
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return os.WriteFile(filename, p.Body, 0600)
-}
+var u Users
 
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
+func home_page(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		u.add(r.FormValue("name"), r.FormValue("email"))
 	}
-	return &Page{Title: title, Body: body}, nil
+
+	tmpl, _ := template.ParseFiles("index.html")
+	tmpl.Execute(w, u)
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", p)
-}
+func (u *Users) add(name string, email string) {
 
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
-	}
+	t := time.Now()
+	i := len(*u) + 1
+	*u = append(*u, User{Index: i, Name: name, Email: email, Date: t, Dates: t.Format("02.01.2006")})
 }
 
 func main() {
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", home_page)
+	http.ListenAndServe(":8080", nil)
 }
